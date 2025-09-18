6d7c5a2c33fadf1f794d6c083f14a67224f08543
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-09-18 15:25:29 +0200

    Implement of person

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+
+public class Person {
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
